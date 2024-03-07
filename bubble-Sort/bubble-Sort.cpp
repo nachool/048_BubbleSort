71703ba1e7296c3cc09f21e929ac575f9ac02461
nachool <nachma.olivia1394@gmail.com>
2024-03-07 21:43:50 +0700

    menambahkan library standar

--- bubble-Sort/bubble-Sort.cpp
+++ bubble-Sort/bubble-Sort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 int arr[20];        //deklarasi variabel global array a dengan ukuran 20
 int n;              //deklarasi variabel n untuk menyimpan banyaknya elemen pada array
