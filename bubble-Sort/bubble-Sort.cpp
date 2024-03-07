4431a0069bf98009032f3b952a57c02357bce7ce
nachool <nachma.olivia1394@gmail.com>
2024-03-07 21:39:45 +0700

    menambahkan variabel global

--- bubble-Sort/bubble-Sort.cpp
+++ bubble-Sort/bubble-Sort.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+int arr[20];        //deklarasi variabel global array a dengan ukuran 20
+int n;              //deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+
 int main()
 {
     
