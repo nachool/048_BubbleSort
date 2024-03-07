e2173ffdc16e23a2dd9523bd5c27a6ca9aa508c2
nachool <nachma.olivia1394@gmail.com>
2024-03-07 21:49:44 +0700

    menghapus satu tanda = pada line 53 dan 55

--- bubble-Sort/bubble-Sort.cpp
+++ bubble-Sort/bubble-Sort.cpp
@@ -50,9 +50,9 @@ void BubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubbl
 
 void display() {
     cout << endl;
-    cout << "===================================" << endl;
+    cout << "==================================" << endl;
     cout << "Element Array yang telah tersusun" << endl;
-    cout << "===================================" << endl;
+    cout << "==================================" << endl;
     cout << endl;
     for (int j = 0; j < n; j++) {
         cout << arr[j];
